eafc570d4f21256bd4e0ae77ca035c5a69105477
2sseul <iseul0626@gmail.com>
2024-06-10 10:36:15 +0900

    정규성 검사 추가

--- 태그연습/input/script.js
+++ 태그연습/input/script.js
@@ -1,3 +1,56 @@
+//id 정규성 검사
+
+let idCheckAlert = document.querySelector('.id-alert');
+let idCheckOk = document.querySelector('.id-ok');
+    
+function getId() {
+    let id = document.getElementById('id').value;
+    if(id.length == 0){
+        idCheckAlert.classList.remove('id-alert-view');
+        idCheckAlert.classList.add('id-alert');
+        idCheckOk.classList.remove('id-ok-view');
+        idCheckOk.classList.add('id-ok');        
+    }else{
+        if(id.length >= 4 && id.length <= 12 && /^[A-Za-z0-9][A-Za-z0-9]*$/.test(id)){
+            idCheckAlert.classList.remove('id-alert-view');
+            idCheckAlert.classList.add('id-alert');
+            idCheckOk.classList.add('id-ok-view');
+            idCheckOk.classList.remove('id-ok');
+        }else{
+            idCheckOk.classList.remove('id-ok-view');
+            idCheckOk.classList.add('id-ok');
+            idCheckAlert.classList.add('id-alert-view');
+            idCheckAlert.classList.remove('id-alert');
+        }
+    }
+}
+
+//비밀번호 정규성 검사
+let pwdCheckAlert = document.querySelector('.pwd-alert');
+let pwdCheckOk = document.querySelector('.pwd-ok');
+
+function getPwd(){
+    let pwd = document.getElementById('password').value;
+    if(pwd.length == 0){
+        pwdCheckAlert.classList.remove('pwd-alert-view');
+        pwdCheckAlert.classList.add('pwd-alert');
+        pwdCheckOk.classList.remove('pwd-ok-view');
+        pwdCheckOk.classList.add('pwd-ok');
+    }else{
+        if(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(pwd)){
+            pwdCheckAlert.classList.remove('pwd-alert-view');
+            pwdCheckAlert.classList.add('pwd-alert');
+            pwdCheckOk.classList.add('pwd-ok-view');
+            pwdCheckOk.classList.remove('pwd-ok');
+        }else{
+            pwdCheckOk.classList.remove('pwd-ok-view');
+            pwdCheckOk.classList.add('pwd-ok');
+            pwdCheckAlert.classList.add('pwd-alert-view');
+            pwdCheckAlert.classList.remove('pwd-alert');
+        }
+    }
+}
+
 //한국 표준시
 let kst = new Date();
 console.log(kst);
@@ -35,6 +88,7 @@ for(let i=1; i<=31; i++){
     day.appendChild(days);
 }
 
+//국적
 let country = document.querySelector('.country');
 let arr = ['대한민국', '미국', '일본', '중국', '브라질', '러시아'];
 for(let i=0; i<arr.length; i++){
@@ -42,6 +96,7 @@ for(let i=0; i<arr.length; i++){
     country.insertAdjacentHTML('beforeend', countrys);
 }
 
+//전화번호
 let phone = document.querySelector('.phone');
 let num = ['010', '011', '012', '016'];
 for(let i=0; i<num.length; i++){
@@ -49,5 +104,9 @@ for(let i=0; i<num.length; i++){
     phone.insertAdjacentHTML('beforeend', phones);
 }
 
-let id = document.querySelector('.id');
-let idCheck = document.querySelector('id-check');
+//전화번호 정규성 검사
+function getPhone(){
+    let phone1 = document.getElementById('phone1').value;
+    let phone2 = document.getElementById('phone2').vlaue;
+
+}
